87f42873a03a2b6963fc00174d91e756f6fa5049
Adrien Lapasset <aflapasset@gmail.com>
2017-05-04 12:52:24 +0200

    toto

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('.progress').animate({ width: '100%' }, 3000, function() {
 		$('.home').fadeIn('slow');
 	});
-	$( '.navbar-brand' ).click(function() {
-		$('.home').fadeIn('slow');
-	});
+	// $( '.navbar-brand' ).click(function() {
+	// 	$('.home').fadeIn('slow');
+	// });
 });
