e844b73151e030c51a6c1749d7de6f1660e61cb2
Adrien Lapasset <aflapasset@gmail.com>
2017-05-19 11:35:19 +0200

    grabels

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,7 +13,7 @@ portfolio.config(function($stateProvider, $urlRouterProvider) {
 		controller: 'contactCtrl',
 	})
 	.state('project', {
-		url: '/project/:id',
+		url: '/project/:name',
 		templateUrl: 'views/project.html',
 		controller: 'projectCtrl',
 	})
@@ -36,9 +36,10 @@ portfolio.controller('contactCtrl', function($scope) {
 });
 
 portfolio.controller('projectCtrl', function($http, $scope, $stateParams) {
-	var id = $stateParams.id;
+	var name = $stateParams.name;
 	$http.get('js/projects.json').then(function(res) {
-		$scope.project = res.data[id];
+		var result = $.grep(res.data, function(e){ return e.name == name; }); //http://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
+		$scope.project = result[0];
 	});
 	//jquery
 	$('.back-icon').fadeIn(100).css("transform", "translateX(0)");
