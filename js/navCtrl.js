91ca0f899b2dd64c5672966aa75c85c562e4676a
Adrien Lapasset <aflapasset@gmail.com>
2017-07-13 16:04:53 +0200

    header hide on scroll

--- js/navCtrl.js
+++ js/navCtrl.js
@@ -4,7 +4,6 @@ portfolio.controller('navCtrl', function($scope, animations, $translate) {
 	};
 	$scope.backShow = animations.backShow;
 
-	console.log($translate.use());
 	$('.en').addClass('show');
 
 	$scope.changeLanguage = function(langKey) {
@@ -19,4 +18,41 @@ portfolio.controller('navCtrl', function($scope, animations, $translate) {
 
 		$translate.use(langKey);
 	};
+
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('nav').outerHeight();
+
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
+
+	function hasScrolled() {
+		var st = $(this).scrollTop();
+	  // Make sure they scroll more than delta
+	  if(Math.abs(lastScrollTop - st) <= delta)
+	  	return;
+
+	  // If they scrolled down and are past the navbar, add class .nav-up.
+	  // This is necessary so you never see what is "behind" the navbar.
+	  if (st > lastScrollTop && st > navbarHeight) {
+	  	// Scroll Down
+	  	$('nav').removeClass('nav-down').addClass('nav-up');
+	  } else {
+	    // Scroll Up
+	    if(st + $(window).height() < $(document).height()) {
+	    	$('nav').removeClass('nav-up').addClass('nav-down');
+	    }
+	  }
+	  lastScrollTop = st;
+	}
 });
