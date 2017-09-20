4180051874ba81d4c73fecfa165422927b153170
Adrien Lapasset <aflapasset@gmail.com>
2017-09-20 17:20:58 +0200

    animation paufinage

--- js/portfolio.js
+++ js/portfolio.js
@@ -50,13 +50,14 @@ portfolio.controller('aboutCtrl', function($scope) {
 		var mouseX;
 		var mouseY;
 		var greyscale;
-		var alpha = 100;
+		var alpha = 80;
 		$(document).mousemove( function(e) {
+			// le zéro est centré au millieu de l'écran
 			mouseX = (e.pageX - $(window).width() / 2) / alpha; 
 			mouseY = (e.pageY - $(window).height() / 2) / alpha;
-			greyscale = (-e.pageY + $(window).height()) / $(window).height() * 100;
-			$(".portrait").css({"transform": "rotate3d(0, 1, 0, " + mouseX + "deg) rotate3d(-1, 0, 0, " + mouseY + "deg) translate3d(" + mouseX + "px, " + mouseY + "px, 0)",
-													"filter": "grayscale(" + greyscale + "%)"});
+
+			// 50% when mouse is on top, 0% on bottom 
+			$(".portrait").css({"transform": "rotate3d(0, 1, 0, " + mouseX + "deg) rotate3d(-1, 0, 0, " + mouseY + "deg) translate3d(" + mouseX/2 + "px, " + mouseY/2 + "px, 0)"});
 		});
 	});	
 });
