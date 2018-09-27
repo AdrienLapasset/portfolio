216a37c2c1dd006bdfa6ba062105aa2462eeb62e
Adrien Lapasset <aflapasset@gmail.com>
2018-09-27 15:14:58 +0200

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
-    <div class="navbar">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> ]
-			<router-link to="/contact">Contact</router-link>
-    </div>
+		<header>
+			<nav class="navbar">
+				<router-link to="/">Home</router-link> 
+				<router-link to="/about">
+					<my-logo />
+				</router-link> 
+				<router-link to="/contact">
+				</router-link>
+			</nav>
+		</header>
     <router-view/>
   </div>
 </template>
 
+<script>
+import MyLogo from "./components/icons/MyLogo.vue"
+
+export default {
+	components: {
+		MyLogo
+  }
+}
+</script>
+
+
 <style lang="scss">
-// @import "sass/main.scss";
+@import "scss/_base.scss";
+// @import "scss/_navbar.scss";
 </style>
