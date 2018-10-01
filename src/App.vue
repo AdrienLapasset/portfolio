9b92527d1120c763716f1e531e4ce6ce5f445a73
Adrien Lapasset <aflapasset@gmail.com>
2018-10-01 12:25:14 +0200

    header style

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,61 @@
 <template>
   <div id="app">
-		<header>
-			<nav class="navbar">
-				<router-link to="/">Home</router-link> 
-				<router-link to="/about">
-					<my-logo />
-				</router-link> 
-				<router-link to="/contact">
-				</router-link>
-			</nav>
+		<header class="header">
+			<!-- <router-link to="/">Home</router-link>  -->
+			<router-link to="/about" class="header__about">
+				<my-logo /><h1 class="header__about__name">Adrien Lapasset</h1><span class="header__about__skills">Webdesign / Développement front-end</span>
+			</router-link> 
+			<router-link to="/contact" class="header__contact">
+				<!-- <span class="text">Contact</span> -->
+				<img class="icon contact-icon" src="./assets/icons/contact.svg">
+			</router-link>
 		</header>
-    <router-view/>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
-import MyLogo from "./components/icons/MyLogo.vue"
+import MyLogo from "./components/icons/MyLogo.vue";
 
 export default {
-	components: {
-		MyLogo
+  components: {
+    MyLogo
   }
-}
+};
 </script>
 
 
 <style lang="scss">
 @import "scss/_base.scss";
-// @import "scss/_navbar.scss";
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  padding: 0 1em;
+
+  &__about {
+    display: flex;
+    align-items: center;
+
+    &__name {
+      font-size: 18px;
+    }
+
+    &__skills {
+      font-size: 9px;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      color: $grey;
+      margin-left: 12px;
+      position: relative;
+      top: 2px;
+    }
+	}
+	
+	&__contact {
+		display: flex;
+    align-items: center;
+	}
+}
 </style>
