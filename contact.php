dd1bb5d6ab00a176cd3c9de142a8f60c0fee75af
Adrien Lapasset <aflapasset@gmail.com>
2018-10-03 13:29:55 +0200

    fix sending mail

--- contact.php
+++ contact.php
@@ -1,17 +1,13 @@
-<?php //https://stackoverflow.com/questions/18379238/send-email-with-php-from-html-form-on-submit-with-the-same-script
-$to = "aflapasset@gmail.com"; // this is your Email address
-$from = $_POST['email']; // this is the sender's Email address
-$name = $_POST['name'];
-$subject = "Contact";
-$subject2 = "Copy of your contact form";
-$message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
-$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-$headers = "From:" . $from;
-$headers2 = "From:" . $to;
-mail($to,$subject,$message,$headers);
-mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-// echo "<div class='sent'><p>Mail Sent. Thank you " . $name . ", I will contact you shortly.</p></div>";
-header('Location: /');
-exit();
+<?php
+	ini_set( 'display_errors', 1 );
+	error_reporting( E_ALL );
+	$to = "aflapasset@gmail.com";
+	$subject = "Contact freelance";
+	$email = $_POST['email'];
+	$name = $_POST['name'];
+	$message = "Nom: " . $name . "\n\n" . "Email: " . $email . "\n\n" . "Message: " . "\n" . $_POST['message'];
+	$headers = "From:" . "mail@adrienlapasset.fr";
+	mail($to,$subject,$message,$headers);
+	header('Location: /');
+	exit();
 ?>
