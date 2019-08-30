6a65bf48a5a8e7fe4ec3e3e4076295eddd300321
Adrien Lapasset <aflapasset@gmail.com>
2019-08-30 14:31:59 +0200

    add font

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,11 @@ import logoAdrienLapasset from '../../assets/logos/logo-adrien-lapasset.svg';
 
 function Header() {
 	return (
-		<header>
-			<img src={logoAdrienLapasset} alt="Logo Adrien Lapasset" />
-			Adrien Lapasset
+		<header className="header">
+			<div className="header__logo">
+				<img className="header__logo__icon" src={logoAdrienLapasset} alt="Logo Adrien Lapasset" />
+				Adrien Lapasset
+			</div>
 		</header>
 	);
 }
