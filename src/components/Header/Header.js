52bc013b229510f472a41b2f4a13d8af10ee8a6c
Adrien Lapasset <aflapasset@gmail.com>
2019-08-30 14:14:34 +0200

    add my logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Header.scss';
+import logoAdrienLapasset from '../../assets/logos/logo-adrien-lapasset.svg';
 
 function Header() {
-	return <header />;
+	return (
+		<header>
+			<img src={logoAdrienLapasset} alt="Logo Adrien Lapasset" />
+			Adrien Lapasset
+		</header>
+	);
 }
 
 export default Header;
