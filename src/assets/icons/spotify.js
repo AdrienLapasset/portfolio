47244d08ec30d4250718e58fdf9efa3595a204eb
Adrien Lapasset <aflapasset@gmail.com>
2019-09-02 18:56:48 +0200

    clean code

--- src/assets/icons/spotify.js
+++ src/assets/icons/spotify.js
@@ -10,19 +10,19 @@ function spotify() {
 			xmlns="http://www.w3.org/2000/svg"
 			xmlnsXlink="http://www.w3.org/1999/xlink"
 		>
-			<g fill="none" stroke-linecap="round" transform="translate(1, 1)">
-				<circle stroke-width="2" stroke-linejoin="round" cx="10" cy="10" r="10" />
+			<g fill="none" strokeLinecap="round" transform="translate(1, 1)">
+				<circle strokeWidth="2" strokeLinejoin="round" cx="10" cy="10" r="10" />
 				<path
 					d="M14.586,10.8 C13.0830667,10.1564474 11.4972788,9.72692814 9.875,9.524 C8.24401912,9.39554666 6.6032216,9.48262997 4.995,9.783"
-					stroke-width="1.7"
+					strokeWidth="1.7"
 				/>
 				<path
 					d="M13.175,13.838 C12.127763,13.3627434 11.0120028,13.0559599 9.869,12.929 C8.44738927,12.8421635 7.02076011,12.8893718 5.608,13.07"
-					stroke-width="1.5"
+					strokeWidth="1.5"
 				/>
 				<path
 					d="M4.173,6.284 C6.3419473,5.89538156 8.55267521,5.79451184 10.748,5.984 C12.7080203,6.1622825 14.6191941,6.69598796 16.388,7.559"
-					stroke-width="1.9"
+					strokeWidth="1.9"
 				/>
 			</g>
 		</svg>
