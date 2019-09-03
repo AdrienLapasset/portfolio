e2cd356da328c524bb0a62bf6c99470a834a6680
Adrien Lapasset <aflapasset@gmail.com>
2019-09-03 19:01:49 +0200

    add background animations

--- src/components/Illustration/parts/Background/Background.js
+++ src/components/Illustration/parts/Background/Background.js
@@ -12,40 +12,103 @@ function Background() {
 			xmlnsXlink="http://www.w3.org/1999/xlink"
 			className="Background"
 		>
-			<defs>
-				<polygon id="path-1" points="0 600.145 503.151 600.145 503.151 0 0 0" />
-			</defs>
-			<g stroke="#000" stroke-width="4" stroke-linecap="round" fill="none" fill-rule="evenodd">
+			<g stroke="#000" strokeWidth="4" strokeLinecap="round" fill="none" fillRule="evenodd">
+				{/* CODE BLOCK */}
+
 				<path
-					d="M378.4965,309.3637 L481.1515,309.3637 C492.1515,309.3637 501.1515,300.3637 501.1515,289.3637 L501.1515,21.9997 C501.1515,10.9997 492.1515,1.9997 481.1515,1.9997 L290.1515,1.9997 C279.1515,1.9997 270.1515,10.9997 270.1515,21.9997 L270.1515,106.2337"
+					className="codeBlock__contour"
+					d="M270.1515,106.2337 L270.1515,21.9997 C270.1515,10.9997 279.1515,1.9997 290.1515,1.9997 L481.1515,1.9997 C492.1515,1.9997 501.1515,10.9997 501.1515,21.9997 L501.1515,289.3637 C501.1515,300.3637 492.1515,309.3637 481.1515,309.3637 L378.4965,309.3637"
 					id="Stroke-1"
 				/>
-				<path
-					d="M160.591,298.6817 L21.486,298.6817 C10.769,298.6817 2,307.4507 2,318.1677 L2,578.6587 C2,589.3767 10.769,598.1447 21.486,598.1447 L207.576,598.1447 C218.294,598.1447 227.062,589.3767 227.062,578.6587 L227.062,487.8217"
-					id="Stroke-3"
+				<polyline
+					className="codeBlock__contour"
+					id="Stroke-5"
+					points="306.2945 48.2341 293.6985 35.6381 305.9485 23.3881"
 				/>
-				<polyline id="Stroke-5" points="306.2945 48.2341 293.6985 35.6381 305.9485 23.3881" />
-				<path d="M316.5208,45.8111 L335.3598,26.9721" id="Stroke-7" />
-				<polyline id="Stroke-9" points="449.3152 256.0347 461.6562 268.3757 449.3202 280.7127" />
-				<path d="M301.1479,74.7341 L359.2029,74.7341" id="Stroke-11" />
-				<path d="M334.1632,45.7341 L466.3302,45.7341" id="Stroke-13" />
-				<path d="M300.2899,91.2341 L391.7029,91.2341" id="Stroke-15" />
-				<path d="M300.2899,108.2341 L374.7029,108.2341" id="Stroke-17" />
+				<path className="codeBlock__contour" d="M316.5208,45.8111 L335.3598,26.9721" id="Stroke-7" />
+				<polyline
+					className="codeBlock__contour"
+					id="Stroke-9"
+					points="449.3152 256.0347 461.6562 268.3757 449.3202 280.7127"
+				/>
+				<path className="codeBlock__contour" d="M301.1479,74.7341 L359.2029,74.7341" id="Stroke-11" />
+				<path className="codeBlock__contour" d="M334.1632,45.7341 L466.3302,45.7341" id="Stroke-13" />
+				<path className="codeBlock__contour" d="M300.2899,91.2341 L391.7029,91.2341" id="Stroke-15" />
 				<path
+					className="codeBlock__contour"
+					d="M413.2466,176.619 C399.4966,176.619 412.6636,156.781 395.6536,156.862 C411.7336,156.862 400.1536,136.943 413.9036,136.943"
+					id="Stroke-53"
+				/>
+				<path className="codeBlock__contour" d="M300.2899,108.2341 L374.7029,108.2341" id="Stroke-17" />
+				<path
+					className="codeBlock__contour"
 					d="M324.6458,144.2833 C324.6458,144.2833 316.5208,147.9033 316.5208,156.1533 C316.5208,164.4033 324.6458,169.2783 324.6458,169.2783"
 					id="Stroke-19"
 				/>
 				<path
+					className="codeBlock__contour"
 					d="M372.434,169.2787 C372.434,169.2787 380.559,165.6587 380.559,157.4087 C380.559,149.1587 372.434,144.2837 372.434,144.2837"
 					id="Stroke-21"
 				/>
-				<path d="M329.0208,156.781 L366.3298,156.781" id="Stroke-23" />
-				<path d="M351.6632,198.7758 L351.6632,262.6928" id="Stroke-25" />
-				<path d="M406.6289,230.7341 L364.2859,230.7341" id="Stroke-27" />
-				<path d="M445.9071,212.2341 L363.6501,212.2341" id="Stroke-29" />
-				<path d="M2,339.9261 L174.386,339.9261" id="Stroke-31" />
-				<path d="M21.3653,329.4797 L40.8263,310.0187" id="Stroke-33" />
-				<path d="M21.3653,310.0184 L40.8263,329.4794" id="Stroke-35" />
+				<path className="codeBlock__contour" d="M329.0208,156.781 L366.3298,156.781" id="Stroke-23" />
+				<path className="codeBlock__contour" d="M351.6632,198.7758 L351.6632,262.6928" id="Stroke-25" />
+				<path className="codeBlock__contour" d="M406.6289,230.7341 L364.2859,230.7341" id="Stroke-27" />
+				<path className="codeBlock__contour" d="M445.9071,212.2341 L363.6501,212.2341" id="Stroke-29" />
+
+				{/* FIRST LINE */}
+				<path className="line__first" d="M452.1731,310.3175 L452.1731,482.5675" id="Stroke-69" />
+				<path
+					className="line__first__point"
+					d="M461.9145,485.2095 C461.9145,490.5895 457.5535,494.9505 452.1735,494.9505 C446.7935,494.9505 442.4315,490.5895 442.4315,485.2095 C442.4315,479.8295 446.7935,475.4685 452.1735,475.4685 C457.5535,475.4685 461.9145,479.8295 461.9145,485.2095"
+					id="Fill-68"
+					fill="#000"
+					stroke="none"
+				/>
+
+				{/* CUBE */}
+				<polygon
+					className="codeBlock__contour"
+					id="Stroke-73"
+					strokeLinejoin="round"
+					points="415.5568 523.166 452.1728 502.799 488.7898 523.166 452.1728 544.541"
+				/>
+				<g className="codeBlock__contour" id="Group-76">
+					<g transform="translate(452.000000, 523.145000)">
+						<polyline
+							className="cube"
+							id="Fill-74"
+							stroke="none"
+							fill="#000"
+							fillRule="evenodd"
+							points="0.1731 21.3965 0.1731 62.0895 36.7891 39.6155 36.7891 0.0215"
+						/>
+						<polyline
+							id="Stroke-75"
+							fill="none"
+							strokeLinejoin="round"
+							points="0.1731 21.3965 0.1731 62.0895 36.7891 39.6155 36.7891 0.0215"
+						/>
+					</g>
+					<polyline id="Stroke-77" points="452.1731 585.2341 415.5571 562.7601 415.5571 523.1661" />
+				</g>
+
+				{/* SECOND LINE */}
+				<path
+					className="line__second"
+					d="M230.3575,543.046 L405.1555,543.046"
+					id="Stroke-71"
+					strokeLinejoin="round"
+				/>
+
+				{/* SCREEN BLOCK */}
+				<path
+					className="codeBlock__contour"
+					d="M160.591,298.6817 L21.486,298.6817 C10.769,298.6817 2,307.4507 2,318.1677 L2,578.6587 C2,589.3767 10.769,598.1447 21.486,598.1447 L207.576,598.1447 C218.294,598.1447 227.062,589.3767 227.062,578.6587 L227.062,487.8217"
+					id="screen-block"
+				/>
+				<path className="" d="M21.3653,329.4797 L40.8263,310.0187" id="Stroke-33" />
+				<path className="" d="M21.3653,310.0184 L40.8263,329.4794" id="Stroke-35" />
+				<path className="" d="M2,339.9261 L174.386,339.9261" id="Stroke-31" />
 				<polyline
 					id="Stroke-37"
 					points="126.6458 359.4534 20.4478 359.4534 20.4478 402.7734 129.5688 402.7734"
@@ -69,10 +132,6 @@ function Background() {
 				<polygon id="Stroke-47" points="160.591 512.541 160.591 536.673 178.546 524.607" />
 				<path d="M34.7373,374.8902 L75.0083,374.8902" id="Stroke-49" />
 				<path d="M34.7373,388.5303 L153.6013,388.5303" id="Stroke-51" />
-				<path
-					d="M413.2466,176.619 C399.4966,176.619 412.6636,156.781 395.6536,156.862 C411.7336,156.862 400.1536,136.943 413.9036,136.943"
-					id="Stroke-53"
-				/>
 				<path d="M103.5572,285.8637 L103.5572,198.7757" id="Stroke-55" />
 
 				<g id="Clip-58" />
@@ -81,7 +140,7 @@ function Background() {
 					fill="#000"
 					points="73.946 200.539 133.668 200.539 133.668 54.626 73.946 54.626"
 				/>
-				<path d="M93.3905,186.3652 L113.7235,186.3652" id="Stroke-59" stroke="#FFF" mask="url(#mask-2)" />
+				<path d="M93.3905,186.3652 L113.7235,186.3652" id="Stroke-59" stroke="#FFF" />
 				<path
 					d="M103.5572,156.0626 C108.5742,156.0626 112.6402,160.1296 112.6402,165.1456 C112.6402,170.1626 108.5742,174.2296 103.5572,174.2296 C98.5402,174.2296 94.4742,170.1626 94.4742,165.1456 C94.4742,160.1296 98.5402,156.0626 103.5572,156.0626 Z"
 					id="Stroke-60"
@@ -89,8 +148,8 @@ function Background() {
 				/>
 				<path d="M93.3905,144.7336 L113.7235,144.7336" id="Stroke-61" stroke="#FFFFFE" />
 				<path d="M93.3905,100.8652 L113.7235,100.8652" id="Stroke-62" stroke="#FFFFFE" />
-				<path d="M93.3905,86.5318 L113.7235,86.5318" id="Stroke-63" stroke="#FFFFFE" mask="url(#mask-2)" />
-				<path d="M93.3905,71.5318 L113.7235,71.5318" id="Stroke-64" stroke="#FFFFFE" mask="url(#mask-2)" />
+				<path d="M93.3905,86.5318 L113.7235,86.5318" id="Stroke-63" stroke="#FFFFFE" />
+				<path d="M93.3905,71.5318 L113.7235,71.5318" id="Stroke-64" stroke="#FFFFFE" />
 				<path
 					d="M103.5572,114.431 C108.5742,114.431 112.6402,118.498 112.6402,123.514 C112.6402,128.531 108.5742,132.598 103.5572,132.598 C98.5402,132.598 94.4742,128.531 94.4742,123.514 C94.4742,118.498 98.5402,114.431 103.5572,114.431 Z"
 					id="Stroke-65"
@@ -103,44 +162,6 @@ function Background() {
 					fill="#000"
 					stroke="none"
 				/>
-				<path
-					d="M461.9145,485.2095 C461.9145,490.5895 457.5535,494.9505 452.1735,494.9505 C446.7935,494.9505 442.4315,490.5895 442.4315,485.2095 C442.4315,479.8295 446.7935,475.4685 452.1735,475.4685 C457.5535,475.4685 461.9145,479.8295 461.9145,485.2095"
-					id="Fill-68"
-					fill="#000"
-					stroke="none"
-				/>
-				<path d="M452.1731,310.3175 L452.1731,492.5675" id="Stroke-69" mask="url(#mask-2)" />
-				<path d="M402.7465,543.046 L396.7465,543.046" id="Stroke-70" stroke-linejoin="round" />
-				<path
-					d="M384.1555,543.046 L239.3575,543.046"
-					id="Stroke-71"
-					stroke-linejoin="round"
-					stroke-dasharray="12.591,12.591"
-				/>
-				<path d="M233.0623,543.046 L227.0623,543.046" id="Stroke-72" stroke-linejoin="round" />
-				<polygon
-					id="Stroke-73"
-					stroke-linejoin="round"
-					points="415.5568 523.166 452.1728 502.799 488.7898 523.166 452.1728 544.541"
-				/>
-				<g id="Group-76" mask="url(#mask-2)">
-					<g transform="translate(452.000000, 523.145000)">
-						<polyline
-							id="Fill-74"
-							stroke="none"
-							fill="#000"
-							fill-rule="evenodd"
-							points="0.1731 21.3965 0.1731 62.0895 36.7891 39.6155 36.7891 0.0215"
-						/>
-						<polyline
-							id="Stroke-75"
-							fill="none"
-							stroke-linejoin="round"
-							points="0.1731 21.3965 0.1731 62.0895 36.7891 39.6155 36.7891 0.0215"
-						/>
-					</g>
-				</g>
-				<polyline id="Stroke-77" points="452.1731 585.2341 415.5571 562.7601 415.5571 523.1661" />
 			</g>
 		</svg>
 	);
