652cf83aca8563174cb9f26b89b12dc859113f1a
Adrien Lapasset <aflapasset@gmail.com>
2019-09-10 15:23:58 +0200

    reload page when click on logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.scss';
 import logoAdrienLapasset from '../../assets/logos/logo-adrien-lapasset.svg';
 import GithubIcon from '../../assets/icons/github.js';
 import GitlabIcon from '../../assets/icons/gitlab.js';
 import SpotifyIcon from '../../assets/icons/spotify.js';
 
-function Header() {
-	return (
-		<header className="header">
-			<div className="header__logo">
-				<img className="header__logo__icon" src={logoAdrienLapasset} alt="Logo Adrien Lapasset" />
-				Adrien Lapasset
-			</div>
-			<div className="header__links">
-				<a href="https://github.com/AdrienLapasset" target="_blank" rel="noopener noreferrer">
-					<GithubIcon />
-				</a>
-				<a href="https://gitlab.com/aflapasset" target="_blank" rel="noopener noreferrer">
-					<GitlabIcon />
-				</a>
-				<a
-					href="https://open.spotify.com/user/oi47w80i0mijkwv6csawghdmo?si=-oyhqeCdQGmzHO5XWQk_2w"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<SpotifyIcon />
-				</a>
-			</div>
-		</header>
-	);
+class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.onClickLogo = this.onClickLogo.bind(this);
+	}
+
+	onClickLogo() {
+		window.location.reload();
+	}
+	render() {
+		return (
+			<header className="header">
+				<div className="header__logo" onClick={this.onClickLogo}>
+					<img className="header__logo__icon" src={logoAdrienLapasset} alt="Logo Adrien Lapasset" />
+					Adrien Lapasset
+				</div>
+				<div className="header__links">
+					<a href="https://github.com/AdrienLapasset" target="_blank" rel="noopener noreferrer">
+						<GithubIcon />
+					</a>
+					<a href="https://gitlab.com/aflapasset" target="_blank" rel="noopener noreferrer">
+						<GitlabIcon />
+					</a>
+					<a
+						href="https://open.spotify.com/user/oi47w80i0mijkwv6csawghdmo?si=-oyhqeCdQGmzHO5XWQk_2w"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<SpotifyIcon />
+					</a>
+				</div>
+			</header>
+		);
+	}
 }
 
 export default Header;
